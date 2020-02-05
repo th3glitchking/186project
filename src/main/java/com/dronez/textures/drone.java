cdff7884e9788349413b33aca60a4af05384a14a
CThompson01 <CThompson10547@gmail.com>
2020-02-05 09:43:34 -0600

    Fixed Drone Model
    Fixed the orientation of the drone model so that it isn't turned the wrong way and just flying sideways. Added actual textures to the drone.

--- src/main/java/com/dronez/textures/drone.java
+++ src/main/java/com/dronez/textures/drone.java
@@ -16,6 +16,12 @@ public class custom_model extends ModelBase {
 	private final ModelRenderer flying_2;
 	private final ModelRenderer holdy_bits2;
 	private final ModelRenderer blades2;
+	private final ModelRenderer flying_3;
+	private final ModelRenderer holdy_bits3;
+	private final ModelRenderer blades3;
+	private final ModelRenderer flying_4;
+	private final ModelRenderer holdy_bits4;
+	private final ModelRenderer blades4;
 
 	public custom_model() {
 		textureWidth = 32;
@@ -23,16 +29,18 @@ public class custom_model extends ModelBase {
 
 		bb_main = new ModelRenderer(this);
 		bb_main.setRotationPoint(0.0F, 24.0F, 0.0F);
+		setRotationAngle(bb_main, 0.0F, 1.5708F, 0.0F);
 		bb_main.cubeList.add(new ModelBox(bb_main, 0, 0, -4.9983F, -2.0122F, -3.0105F, 10, 2, 6, 0.0F, false));
 
 		flying_1 = new ModelRenderer(this);
 		flying_1.setRotationPoint(0.0F, 24.0F, 0.0F);
+		setRotationAngle(flying_1, 0.0F, 1.5708F, 0.0F);
 
 		holdy_bits = new ModelRenderer(this);
 		holdy_bits.setRotationPoint(0.0F, 0.0F, 0.0F);
 		flying_1.addChild(holdy_bits);
-		holdy_bits.cubeList.add(new ModelBox(holdy_bits, 27, 8, -8.0F, -4.0F, 4.0F, 1, 1, 1, 0.0F, false));
-		holdy_bits.cubeList.add(new ModelBox(holdy_bits, 26, 8, -7.0F, -3.0F, 3.0F, 2, 1, 1, 0.0F, false));
+		holdy_bits.cubeList.add(new ModelBox(holdy_bits, 0, 29, -8.0F, -4.0F, 4.0F, 1, 1, 1, 0.0F, false));
+		holdy_bits.cubeList.add(new ModelBox(holdy_bits, 0, 29, -7.0F, -3.0F, 3.0F, 2, 1, 1, 0.0F, false));
 
 		blades = new ModelRenderer(this);
 		blades.setRotationPoint(0.0F, 0.0F, 0.0F);
@@ -41,18 +49,47 @@ public class custom_model extends ModelBase {
 
 		flying_2 = new ModelRenderer(this);
 		flying_2.setRotationPoint(0.0F, 24.0F, 0.0F);
-		setRotationAngle(flying_2, 0.0F, 3.1416F, 0.0F);
+		setRotationAngle(flying_2, 0.0F, -1.5708F, 0.0F);
 
 		holdy_bits2 = new ModelRenderer(this);
 		holdy_bits2.setRotationPoint(0.0F, 0.0F, 0.0F);
 		flying_2.addChild(holdy_bits2);
-		holdy_bits2.cubeList.add(new ModelBox(holdy_bits2, 27, 8, -8.0F, -4.0F, 4.0F, 1, 1, 1, 0.0F, false));
-		holdy_bits2.cubeList.add(new ModelBox(holdy_bits2, 26, 8, -7.0F, -3.0F, 3.0F, 2, 1, 1, 0.0F, false));
+		holdy_bits2.cubeList.add(new ModelBox(holdy_bits2, 0, 29, -8.0F, -4.0F, 4.0F, 1, 1, 1, 0.0F, false));
+		holdy_bits2.cubeList.add(new ModelBox(holdy_bits2, 0, 29, -7.0F, -3.0F, 3.0F, 2, 1, 1, 0.0F, false));
 
 		blades2 = new ModelRenderer(this);
 		blades2.setRotationPoint(0.0F, 0.0F, 0.0F);
 		flying_2.addChild(blades2);
 		blades2.cubeList.add(new ModelBox(blades2, 0, 8, -11.0F, -4.0F, 2.0F, 6, 0, 6, 0.0F, false));
+
+		flying_3 = new ModelRenderer(this);
+		flying_3.setRotationPoint(0.0F, 24.0F, 0.0F);
+
+		holdy_bits3 = new ModelRenderer(this);
+		holdy_bits3.setRotationPoint(0.0F, 0.0F, 0.0F);
+		flying_3.addChild(holdy_bits3);
+		holdy_bits3.cubeList.add(new ModelBox(holdy_bits3, 0, 29, -5.0F, -4.0F, 7.0F, 1, 1, 1, 0.0F, false));
+		holdy_bits3.cubeList.add(new ModelBox(holdy_bits3, 0, 29, -4.0F, -3.0F, 5.0F, 1, 1, 2, 0.0F, false));
+
+		blades3 = new ModelRenderer(this);
+		blades3.setRotationPoint(0.0F, 0.0F, 0.0F);
+		flying_3.addChild(blades3);
+		blades3.cubeList.add(new ModelBox(blades3, 0, 8, -8.0F, -4.0F, 5.0F, 6, 0, 6, 0.0F, false));
+
+		flying_4 = new ModelRenderer(this);
+		flying_4.setRotationPoint(0.0F, 24.0F, 0.0F);
+		setRotationAngle(flying_4, 0.0F, 3.1416F, 0.0F);
+
+		holdy_bits4 = new ModelRenderer(this);
+		holdy_bits4.setRotationPoint(0.0F, 0.0F, 0.0F);
+		flying_4.addChild(holdy_bits4);
+		holdy_bits4.cubeList.add(new ModelBox(holdy_bits4, 0, 29, -5.0F, -4.0F, 7.0F, 1, 1, 1, 0.0F, false));
+		holdy_bits4.cubeList.add(new ModelBox(holdy_bits4, 0, 29, -4.0F, -3.0F, 5.0F, 1, 1, 2, 0.0F, false));
+
+		blades4 = new ModelRenderer(this);
+		blades4.setRotationPoint(0.0F, 0.0F, 0.0F);
+		flying_4.addChild(blades4);
+		blades4.cubeList.add(new ModelBox(blades4, 0, 8, -8.0F, -4.0F, 5.0F, 6, 0, 6, 0.0F, false));
 	}
 
 	@Override
@@ -60,6 +97,8 @@ public class custom_model extends ModelBase {
 		bb_main.render(f5);
 		flying_1.render(f5);
 		flying_2.render(f5);
+		flying_3.render(f5);
+		flying_4.render(f5);
 	}
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
 		modelRenderer.rotateAngleX = x;
