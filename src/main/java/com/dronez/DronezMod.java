cdc24d1a98ca0ef2bdddb5742d804e87b024e663
Brandon Richards <bqrichards@icloud.com>
2020-02-06 01:47:53 -0600

    Textures
    Move the recipe JSON files and texture files to the correct directory

--- src/main/java/com/dronez/DronezMod.java
+++ src/main/java/com/dronez/DronezMod.java
@@ -1,16 +1,14 @@
 package com.dronez;
 
-import com.dronez.entities.EntityFactory;
+import com.dronez.entities.Drone;
 import com.dronez.entities.RenderDroneFactory;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
@@ -24,11 +22,10 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ObjectHolder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import java.util.stream.Collectors;
-import com.dronez.entities.Drone;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("dronez")
@@ -48,9 +45,7 @@ public class DronezMod
     };
 
     public DronezMod() {
-
-
-        //creates the items for crafting
+        // Create the items for crafting
         ironDroneBlade = new Item(new Item.Properties().group(dronezGroup)).setRegistryName("dronez:iron_drone_blade");
         ironDroneShell = new Item(new Item.Properties().group(dronezGroup)).setRegistryName("dronez:iron_drone_shell");
         ironDroneCore = new Item(new Item.Properties().group(dronezGroup)).setRegistryName("dronez:iron_drone_core");
@@ -87,6 +82,7 @@ public class DronezMod
         // some example code to dispatch IMC to another mod
         InterModComms.sendTo("dronez", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
+
     private void processIMC(final InterModProcessEvent event)
     {
         // some example code to receive and process InterModComms from other mods
@@ -94,6 +90,7 @@ public class DronezMod
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
@@ -105,8 +102,6 @@ public class DronezMod
     // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
-
-
         public static EntityType<Drone> drone = null;
 
         private static void generateEntityTypes() {
@@ -124,10 +119,12 @@ public class DronezMod
             // register a new block here
             LOGGER.info("HELLO from Register Block");
         }
+
         @SubscribeEvent
         public static void registerItems(final RegistryEvent.Register<Item> event) {
             event.getRegistry().registerAll(ironDroneBlade,ironDroneShell,ironDroneCore);
         }
+
         @SubscribeEvent
         public static void registerEntities(final RegistryEvent.Register<EntityType<?>> event) {
             LOGGER.debug("Wabbits: Registering Entities...");
@@ -137,7 +134,4 @@ public class DronezMod
             );
         }
     }
-    //Registers all of the items
-
-
 }
