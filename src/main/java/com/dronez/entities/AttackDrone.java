75f4371f180e6fad14feec4977944861c66979cf
th3glitchking <th3glitchking@gmail.com>
2020-04-27 12:05:43 -0500

    Attack Drone AI fixes

--- src/main/java/com/dronez/entities/AttackDrone.java
+++ src/main/java/com/dronez/entities/AttackDrone.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.entity.ai.goal.TargetGoal;
 import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.passive.horse.AbstractHorseEntity;
 import net.minecraft.entity.passive.horse.LlamaEntity;
@@ -28,8 +29,8 @@ public class AttackDrone extends Drone {
     protected void registerGoals() {
         //this is a basic goal registration, I will need to make custom goal classes to have it follow the player or return to charger
         super.registerGoals();
-        //this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
-        //this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
+        this.targetSelector.addGoal(1, new OwnerHurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new OwnerHurtTargetGoal(this));
 
     }
 
@@ -50,8 +51,90 @@ public class AttackDrone extends Drone {
         }
     }
 
+    static class OwnerHurtByTargetGoal extends TargetGoal {
+        private final AttackDrone tameable;
+        private LivingEntity attacker;
+        private int timestamp;
 
+        public OwnerHurtByTargetGoal(AttackDrone theDefendingTameableIn) {
+            super(theDefendingTameableIn, false);
+            this.tameable = theDefendingTameableIn;
+            this.setMutexFlags(EnumSet.of(Goal.Flag.TARGET));
+        }
 
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute() {
+            if (!this.tameable.isCharging()) {
+                LivingEntity livingentity = this.tameable.getOwner();
+                if (livingentity == null) {
+                    return false;
+                } else {
+                    this.attacker = livingentity.getRevengeTarget();
+                    int i = livingentity.getRevengeTimer();
+                    return i != this.timestamp && this.isSuitableTarget(this.attacker, EntityPredicate.DEFAULT) && this.tameable.shouldAttackEntity(this.attacker, livingentity);
+                }
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting() {
+            this.goalOwner.setAttackTarget(this.attacker);
+            LivingEntity livingentity = this.tameable.getOwner();
+            if (livingentity != null) {
+                this.timestamp = livingentity.getRevengeTimer();
+            }
+
+            super.startExecuting();
+        }
+    }
+    public class OwnerHurtTargetGoal extends TargetGoal {
+        private final AttackDrone tameable;
+        private LivingEntity attacker;
+        private int timestamp;
+
+        public OwnerHurtTargetGoal(AttackDrone theEntityTameableIn) {
+            super(theEntityTameableIn, false);
+            this.tameable = theEntityTameableIn;
+            this.setMutexFlags(EnumSet.of(Goal.Flag.TARGET));
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute() {
+            if (!this.tameable.isCharging()) {
+                LivingEntity livingentity = this.tameable.getOwner();
+                if (livingentity == null) {
+                    return false;
+                } else {
+                    this.attacker = livingentity.getLastAttackedEntity();
+                    int i = livingentity.getLastAttackedEntityTime();
+                    return i != this.timestamp && this.isSuitableTarget(this.attacker, EntityPredicate.DEFAULT) && this.tameable.shouldAttackEntity(this.attacker, livingentity);
+                }
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting() {
+            this.goalOwner.setAttackTarget(this.attacker);
+            LivingEntity livingentity = this.tameable.getOwner();
+            if (livingentity != null) {
+                this.timestamp = livingentity.getLastAttackedEntityTime();
+            }
+
+            super.startExecuting();
+        }
+    }
 
 
 
